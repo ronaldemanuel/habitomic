d147e53d5dde2bec7c728f248ba4343697064a07
ronaldemanuel <emronald15el@gmail.com>
2025-05-25 22:56:50 -0300

    feat: update Header component to include status bar height adjustment

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,20 +1,28 @@
 import { Pressable, Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import MotivationalText from "../motivationalText";
+import Constants from "expo-constants";
 
 export default function Header() {
+  const statusBarHeight = Constants.statusBarHeight;
+
   return (
-    <View
-      style={{
-        paddingHorizontal: 15,
-        justifyContent: "space-between",
-        flexDirection: "row",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 26, color: "#00461a" }}>Habitomic</Text>
-      <Pressable>
-        <Ionicons name="add-outline" size={30} color="#00461a" />
-      </Pressable>
+    <View style={{ marginTop: statusBarHeight }}>
+      <View
+        style={{
+          paddingHorizontal: 15,
+          justifyContent: "space-between",
+          flexDirection: "row",
+          alignItems: "center",
+          marginBottom: 10,
+        }}
+      >
+        <Text style={{ fontSize: 26, color: "#00461a" }}>Habitomic</Text>
+        <Pressable>
+          <Ionicons name="add-outline" size={30} color="#00461a" />
+        </Pressable>
+      </View>
+      {/* <MotivationalText /> */}
     </View>
   );
 }
